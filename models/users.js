aebb534860cec8efae7305a57be50e21fbdf9860
vivek <igvivekr@gmail.com>
2025-10-30 22:02:59 +0530

    newesssttt 30 oct 10pm

--- models/users.js
+++ models/users.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/miniprojectdb');
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 const userSchema = mongoose.Schema({
     name : String,
